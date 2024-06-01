28364ce496694793a2b2184819701d4dd2bc589f
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-06-01 07:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+let inp = document.getElementById("evaluatedText");
+
+let ans = document.getElementById("letterCount")
+
+let cnt=0;
+
+inp.addEventListener("keydown",(e)=>{
+    if(e.key!='Backspace')
+        cnt++;
+    else
+        cnt--;
+    
+    if(cnt<0)
+        ans.textContent = `0`;
+    else
+        ans.textContent = `${cnt}`;
+})
