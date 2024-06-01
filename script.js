cd8623c7ee7beebc1c6d5871d6ba0324382409cf
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-06-01 08:11:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-//your code here
 let inp = document.getElementById("evaluatedText");
 
 let ans = document.getElementById("letterCount")
 
-let cnt=0;
 
-inp.addEventListener("keydown",(e)=>{
-    if(e.key!='Backspace')
-        cnt++;
-    else
-        cnt--;
-    
-    if(cnt<0)
-        ans.textContent = `0`;
-    else
-        ans.textContent = `${cnt}`;
+inp.addEventListener("keyup",(e)=>{
+    ans.textContent = `${inp.value.length}`
 })
